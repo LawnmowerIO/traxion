5663a0cf64d402879481a30bd4c14eda3ec3eeb7
Pieter Gorsira <pgorsira@gmail.com>
2016-10-22 19:53:35 -0700

    a bunch of stuff-  playing audio

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,44 +1,6 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
 })
 
 .controller('SongsCtrl', function($scope, SongService, $state) {
@@ -50,13 +12,23 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('SongCtrl', function($scope, $stateParams) {
+.controller('SongCtrl', function($scope, $stateParams, SongService) {
+
+  for (var i = 0; i < SongService.songs.length; i++){
+    console.log(SongService.songId);
+    console.log(SongService.songs[i].id);
+    if (SongService.songs[i].id == SongService.songId){
+      $scope.track = SongService.songs[i];
+      break;
+    }
+  }
+
 })
 
 .service('SongService', function(){
   var self = this;
   this.songs = [
-    { title: 'Reggae', artist: 'Ludacris', id: 1 },
+    { title: 'Rollout', artist: 'Ludacris', url: 'music/1.mp3', art: 'img/1.png' },
     { title: 'Cowbell', artist: 'Young Thug', id: 2 }
   ];
 
