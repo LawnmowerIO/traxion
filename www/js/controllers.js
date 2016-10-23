2cba769496d5e02f7fc1da18542fee46c39f746b
Pieter Gorsira <pgorsira@gmail.com>
2016-10-23 00:23:14 -0700

    metrics on song page almost there

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,6 +14,10 @@ angular.module('starter.controllers', [])
 
 .controller('SongCtrl', function($scope, $stateParams, SongService, $timeout) {
 
+  $scope.numPlays = SongService.getTotalPlays();
+  $scope.equity = SongService.getEquity();
+  $scope.equityPercent = $scope.equity.start;
+
   for (var i = 0; i < SongService.songs.length; i++){
     console.log(SongService.songId);
     console.log(SongService.songs[i].id);
@@ -26,6 +30,23 @@ angular.module('starter.controllers', [])
     }
   }
 
+  var incrementEquity = function(){
+    $scope.equityPercent += $scope.equity.interval;
+    $timeout(incrementEquity, 1000);
+  };
+
+  incrementEquity();
+
+
+  //var incrementPlayCount = function(){
+  //  $scope.numPlays++;
+  //  var nextRun = Math.floor(Math.random() * 4000) + 100;
+  //  $timeout(incrementPlayCount, nextRun);
+  //};
+  //
+  //incrementPlayCount();
+
+
 })
 
   .controller('PortfolioCtrl', function($scope, $stateParams, SongService, $timeout) {
@@ -36,10 +57,7 @@ angular.module('starter.controllers', [])
 
   .service('SongService', function(){
   var self = this;
-  //this.songs = [
-  //  { title: 'Rollout', artist: 'Ludacris', id: 1, url: 'music/1.mp3', art: 'img/1.png' },
-  //  { title: 'Cowbell', artist: 'Young Thug', id: 2 }
-  //];
+
     this.songs = [
       { title: 'Pursuit of Happiness', artist: 'Kid Cudi', id: 1 },
       { title: 'Good Kid, M.A.A.D City', artist: 'Kendrick Lamar', id: 2 },
@@ -62,6 +80,33 @@ angular.module('starter.controllers', [])
       this.songs[i].url = 'music/' + this.songs[i].id + '.mp3';
       this.songs[i].art = 'img/' + this.songs[i].id + '.png';
     }
+
+    this.showBigSong = true;
+
+    this.getTotalPlays = function(){
+      if (this.showBigSong){
+        return Math.floor(Math.random() * 500000) + 600000;
+      } else {
+        return Math.floor(Math.random() * 500) + 100;
+      }
+    };
+
+    this.getEquity = function(){
+      if (this.showBigSong){
+        return {
+          start:.06,
+          end:.09,
+          interval:.001
+        }
+      } else {
+        return {
+          start:.1,
+          end:.3,
+          interval:.01
+        }
+
+      }
+    };
 
     this.songId = 1;
 });
